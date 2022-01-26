2bbd9b1f03b35312668889e2e355081c066bdf30
Mateusz Tatusko <matatusko@gmail.com>
2022-01-26 22:05:26 +0100

    WIP deployment

---
+++ portfolio/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Load Flask instance and related setting
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py', silent=True)
+
+# Import parts of the application
+from .home import home
+from .projects import projects
+from .blog import blog
+
+# Register Blueprints
+app.register_blueprint(home.bp)
+app.register_blueprint(projects.bp)
+app.register_blueprint(blog.bp)
